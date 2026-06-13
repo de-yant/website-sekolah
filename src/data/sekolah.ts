faba49b3e9bad5be664258e83a3f8f9619a5b2ab
de-yant <adeharyantonailunnabhan@gmail.com>
2026-06-13 12:25:42 +0700

    initial

---
+++ src/data/sekolah.ts
@@ -0,0 +1,43 @@
+export const sekolah = {
+  nama: "SMK 2 LPPM RI Majalaya",
+  slogan: "Mencetak Generasi Unggul, Berkarakter dan Kompeten",
+
+  alamat:
+    "Jl. Cidawolong No.7, RW.8, Biru, Kec. Majalaya, Kabupaten Bandung, Jawa Barat 40392",
+  telepon: "(022) 589-1234",
+  Hp: "0851 9607 0615", // wa
+  email: "officialsmk2lppmri@gmail.com",
+
+  kepalaSekolah: "RA. Rosmalinda Aryaningrat Putri Mdl., S.Kom, M.Pd",
+  logo: "/images/logo.png",
+
+  sosialMedia: {
+    instagram: "",
+    youtube: "",
+    facebook: "",
+    tiktok: "",
+    threads: "",
+  },
+
+  statistik: {
+    siswa: 1200,
+    guru: 80,
+    jurusan: 5,
+    prestasi: 20,
+  },
+
+  // Modul konfigurasi lokasi geografis sekolah
+  lokasi: {
+    // Tautan embed resmi Google Maps untuk SMK 2 LPPM RI Majalaya
+    embedUrl:
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3959.510468087965!2d107.78187809999999!3d-7.0666675!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c180caaa9641%3A0x2687ddc3e3d1f286!2sSMK%202%20LPPM%20RI%20Majalaya!5e0!3m2!1sid!2sid!4v1718265000000!5m2!1sid!2sid",
+
+    // Tautan navigasi eksternal jika pengguna ingin membuka langsung di aplikasi Google Maps
+    mapsLink: "https://maps.app.goo.gl/44Rkpi51MaK6AryF6",
+  },
+
+  developer: {
+    nama: "HaryantoLabs",
+    website: "https://adeharyanto.my.id",
+  },
+};
