5d0a4c9a611e30e08d6b6d3f45c3d356ca0966d1
de-yant <adeharyantonailunnabhan@gmail.com>
2026-06-13 14:45:24 +0700

    domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  site: 'https://websekolah.developprin.my.id',
+  site: 'https://websekolah.developerin.my.id',
   base: '/',
 
   vite: {
