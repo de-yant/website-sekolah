7edb5187e1c823f093b1bd1b0717a53a05cd3a2a
Ade Haryanto <118276672+de-yant@users.noreply.github.com>
2026-06-13 13:03:50 +0700

    Add GitHub Actions workflow for deploying to Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://de-yant.github.io/website-sekolah',
+  base: '/website-sekolah/',
+
   vite: {
     plugins: [tailwindcss()]
   }
